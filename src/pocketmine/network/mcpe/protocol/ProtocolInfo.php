6e2d21ed9326fed0f509717e512095310f36551a
ProtectorYT <85945724+DevGod6969@users.noreply.github.com>
2022-02-23 08:15:13 +0530

    vvv

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -39,9 +39,9 @@ interface ProtocolInfo{
 	/** Actual Minecraft: PE protocol version */
 	public const CURRENT_PROTOCOL = 486;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.18.10';
+	public const MINECRAFT_VERSION = 'v1.18.12';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.18.10';
+	public const MINECRAFT_VERSION_NETWORK = '1.18.12';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
