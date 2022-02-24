456113218ebe024e6ec41dd65b147112a3c52e5c
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-02-24 21:45:18 +0200

    Update ProtocolInfo.php

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -39,9 +39,9 @@ interface ProtocolInfo{
 	/** Actual Minecraft: PE protocol version */
 	public const CURRENT_PROTOCOL = 486;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.18.12';
+	public const MINECRAFT_VERSION = 'v1.18.10'; // same as 1.18.11 and 1.18.12
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.18.12';
+	public const MINECRAFT_VERSION_NETWORK = '1.18.10'; // 1.18.11 and 1.18.12 is the same with the protocol 486
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
